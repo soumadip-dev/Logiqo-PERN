48f53193998ac4313d646c5bcb446b514192f438
Soumadip Majila <soumadipmajila@gmail.com>
2024-12-30 12:00:00 +0530

    docs: add comments to playlist.routes file for clarity

--- backend/src/routes/playlist.routes.js
+++ backend/src/routes/playlist.routes.js
@@ -8,22 +8,27 @@ import {
   getPlayListDetails,
   removeProblemFromPlaylist,
 } from '../controllers/playlist.controllers.js';
-import { authMiddleware } from '../middlewares/auth.middlewares';
+import { authMiddleware } from '../middlewares/auth.middlewares.js';
 
 // CREATING ROUTER INSTANCE
 const router = Router();
 
-// GET
+// GET ALL PLAYLISTS ROUTE
 router.get('/', authMiddleware, getAllListDetails);
 
+// GET ALL PROBLEMS IN A PLAYLIST ROUTE
 router.get('/:playlistId', authMiddleware, getPlayListDetails);
 
+// CREATE A PLAYLIST ROUTE
 router.post('/create-playlist', authMiddleware, createPlaylist);
 
+// ADD A PROBLEM TO A PLAYLIST ROUTE
 router.post('/:playlistId/add-problem', authMiddleware, addProblemToPlaylist);
 
+// DELETE A PLAYLIST ROUTE
 router.delete('/:playlistId', authMiddleware, deletePlaylist);
 
+// REMOVE A PROBLEM FROM A PLAYLIST ROUTE
 router.delete(
   '/:playlistId/remove-problem',
   authMiddleware,
