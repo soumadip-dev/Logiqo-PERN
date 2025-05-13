0eabdd6bbb52eef3b7bd19644166523a6b4215d8
Soumadip Majila <soumadipmajila@gmail.com>
2025-05-13 12:00:00 +0530

    feat: set up basic routing using React Router DOM

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import {Routes, Route, Navigate} from 'react-router-dom'
+import HomePage from './page/HomePage'
+import LoginPage from './page/LoginPage'
+import SignUpPage from './page/SignUpPage'
 
 const App = () => {
   return (
-    <div className='text-3xl font-bold text-indigo-300'>App</div>
+    <>
+      <div className='flex flex-col items-center justify-start'>
+        <Routes>
+          <Route path='/' element={<HomePage/>} />
+          <Route path='/login' element={<LoginPage/>} />
+          <Route path='/signup' element={<SignUpPage/>} />
+        </Routes>
+      </div>
+    </>
   )
 }
 
