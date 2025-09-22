7bb195edf613b2b80b22932b46302cfcdffe69bf
soumadip-dev <soumadipmajila@gmail.com>
2025-09-22 12:40:00 +0530

    chore: add placeholder controller functions for problem operations

---
+++ server/src/controller/problem.controllers.js
@@ -0,0 +1,15 @@
+async function createProblem(req, res) {}
+async function getAllProblems(req, res) {}
+async function getProblemById(req, res) {}
+async function updateProblem(req, res) {}
+async function deleteProblem(req, res) {}
+async function getAllProblemsSolvedByUser(req, res) {}
+
+export {
+  createProblem,
+  getAllProblems,
+  getProblemById,
+  updateProblem,
+  deleteProblem,
+  getAllProblemsSolvedByUser,
+};
