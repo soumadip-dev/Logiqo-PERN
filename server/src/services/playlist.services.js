9e8321d2ffe01bc734ddddb76915cda33dd8b2db
soumadip-dev <soumadipmajila@gmail.com>
2025-09-22 13:10:00 +0530

    feat: add service to fetch all playlists for a user

--- server/src/services/playlist.services.js
+++ server/src/services/playlist.services.js
@@ -6,7 +6,7 @@ async function createPlaylistService(userId, name, description) {
   if (!userId || !name) throw new Error('User ID and playlist name are required');
 
   // Create a new playlist
-  const playlist = await db.playlist.create({
+  const playlist = await db.Playlist.create({
     data: { name, description, userId },
   });
 
@@ -20,10 +20,29 @@ async function deletePlaylistService(playlistId) {
   if (!playlistId) throw new Error('Playlist ID is required');
 
   // Delete the playlist from the database
-  const deletedPlayList = await db.playlist.delete({
+  const deletedPlayList = await db.Playlist.delete({
     where: { id: playlistId },
   });
 
   // Return the deleted playlist
   return deletedPlayList;
+}
+
+//* Service to fetch all playlists for a user
+async function getAllPlaylistsService(userId) {
+  // Check if user ID is provided
+  if (!userId) throw new Error('User ID is required');
+
+  // Fetch playlists for the user from the database
+  const playlists = await db.Playlist.findMany({
+    where: { userId },
+    include: {
+      problems: {
+        include: { problem: true },
+      },
+    },
+  });
+
+  // Return the playlists
+  return playlists;
 }
