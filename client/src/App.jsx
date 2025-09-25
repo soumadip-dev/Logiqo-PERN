3dd82f926b0a6355f4ccdf045b33ddc5ca4dff73
soumadip-dev <soumadipmajila@gmail.com>
2025-09-25 13:10:00 +0530

    feat: set up basic routing structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import SignUpPage from './pages/SignUpPage';
 
 const App = () => {
-  return <div>App</div>;
+  const authUser = false;
+  return (
+    <div className="flex flex-col  justify-start">
+      <Routes>
+        <Route path="/" element={authUser ? <HomePage /> : <Navigate to={'/login'} />} />
+        <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to={'/'} />} />
+        <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to={'/'} />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
