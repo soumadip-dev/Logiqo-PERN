61c754be4d6ba9b04470bf34486343a47d6b2165
soumadip-dev <soumadipmajila@gmail.com>
2025-10-15 13:27:53 +0530

    feat: add Zustand store for authentication with checkAuth action

--- client/src/store/useAuthStore.js
+++ client/src/store/useAuthStore.js
@@ -1,1 +1,27 @@
-import 
+import { create } from 'zustand';
+import axiosInstance from '../lib/axiosInstance';
+
+const useAuthStore = create(set => ({
+  authUser: null,
+  isSigningUp: false,
+  isLoggingIn: false,
+  isCheckingAuth: false,
+
+  // Actions to check authentication
+  checkAuth: async () => {
+    set({ isCheckingAuth: true });
+    try {
+      const response = await axiosInstance.get('/auth/check');
+      console.log('checkAuth response:', response);
+
+      set({ authUser: response.data.user });
+    } catch (error) {
+      console.error('Error checking authentication:', error);
+      set({ authUser: null });
+    } finally {
+      set({ isCheckingAuth: false });
+    }
+  },
+}));
+
+export default useAuthStore;
