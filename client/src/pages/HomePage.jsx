4097182933bc6e6fe59ec94072c16d00fb28bb4b
soumadip-dev <soumadipmajila@gmail.com>
2025-10-17 20:56:49 +0530

    feat: complete ProblemsTable component and integrate it into HomePage to display all coding problems

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import Navbar from '../components/Navbar';
 import { useProblemStore } from '../store/useProblemStore';
 import { Loader } from 'lucide-react';
+import ProblemsTable from '../components/ProblemsTable';
 
 const HomePage = () => {
   const { allProblems: problems, isLoadingAllProblems, fetchAllProblems } = useProblemStore();
@@ -12,8 +13,8 @@ const HomePage = () => {
 
   if (isLoadingAllProblems) {
     return (
-      <div className="flex items-center justify-center h-screen">
-        <Loader className="size-10 animate-spin" />
+      <div className="flex items-center justify-center h-screen bg-slate-950 text-white">
+        <Loader className="w-10 h-10 animate-spin" />
       </div>
     );
   }
@@ -48,17 +49,13 @@ const HomePage = () => {
             </h1>
             <p className="mt-4 text-center text-lg font-semibold text-gray-500 dark:text-gray-400 z-10">
               <span className="bg-gradient-to-r from-violet-300 to-purple-300 bg-clip-text text-transparent font-semibold">
-                Logiqo
+                Logiqo{' '}
               </span>
-              is a platform for sharing and solving coding problems. Join our community of
-              developers and enhance your problem-solving skills.
+              is a platform for solving coding problems. Join our community of developers and
+              enhance your problem-solving skills.
             </p>
             {problems && problems.length > 0 ? (
-              problems.map(problem => (
-                <h1 key={problem.id} className="text-lg font-semibold text-red-500">
-                  {problem.title}
-                </h1>
-              ))
+              <ProblemsTable problems={problems} />
             ) : (
               <p className="mt-10 text-center text-lg font-semibold text-gray-500 dark:text-gray-400 z-10 border border-primary px-4 py-2 rounded-md border-dashed">
                 No problems found
